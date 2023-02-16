3c64be9e3af4adac758f43189330898687b9f30a
Mateo Fernandez <mateo.rost@gmail.com>
2023-02-15 21:46:54 -0300

    Agregado mock de tareas e interface de las mismas

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar la task list",
+        day: "Mañana",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Examen de inglés",
+        day: "Mañana",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Trabajar",
+        day: "Hoy",
+        reminder: true
+    }
+]
